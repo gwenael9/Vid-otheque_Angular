f3824019657746d87a9b5ff4f8e0faad138cd0ac
gwenael9 <gwenaelgueho@gmail.com>
2023-04-06 17:25:29 +0200

    06/04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Film } from './models/liste-films.model';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,69 @@ import { Component, OnInit } from '@angular/core';
 
 // J'ai besoin d'implémenter OnInit pour alimenter notre composant à chaque nouvelle instance.
 export class AppComponent implements OnInit {
-  title = 'Videotheque';
+  // je crée une propriété listeFilm lié à ma classe Film (model) sous forme de tableau
+  listeFilms!: Film[];
+
+
+
+  // On lance l'app
   ngOnInit(): void {
+    this.listeFilms = [
+
+      {
+      "titre" : "Avatar",
+      "realisateur" : "James Cameron",
+      "description" : "Film americain de science-fiction, réalisé en 2009",
+      "dureeTotale" : 162,
+      "dureeVisio" : 162,
+      "imgUrl" : "assets/img/affiches_films/avatar.jpg",
+      },
+
+      {
+        "titre" : "Intouchables",
+        "realisateur" : "Olivier Nakache",
+        "description" : "Comédie/drame français sorti en 2011",
+        "dureeTotale" : 112,
+        "dureeVisio" : 112,
+        "imgUrl" : "assets/img/affiches_films/intouchables.jpg",
+      },
+
+      {
+        "titre" : "Django Unchained",
+        "realisateur" : "Quentin Tarantino",
+        "description" : "Western américain sorti en 2013",
+        "dureeTotale" : 165,
+        "dureeVisio" : 0,
+        "imgUrl" : "assets/img/affiches_films/django.jpg",
+      },   
+
+      {
+        "titre" : "Get Out",
+        "realisateur" : "Jordan Peele",
+        "description" : "Ce thriller américain est sorti en 2017, faisant un box-office de 255,4 millions $",
+        "dureeTotale" : 104,
+        "dureeVisio" : 104,
+        "imgUrl" : "assets/img/affiches_films/get_out.jpg",
+      },     
+      
+      {
+        "titre" : "Drunk",
+        "realisateur" : "Thomas Vinterberg", 
+        "description" : "Une comédie dramatique sorti en 2020, au Danemark",
+        "dureeTotale" : 115,
+        "dureeVisio" : 0,
+        "imgUrl" : "assets/img/affiches_films/drunk.jpg",
+      },    
+      
+      {
+        "titre" : "Le loup de Wall Street",
+        "realisateur" : "Martin Scorsese",
+        "description" : "Célèbre comédie dramatique américaine sorti en 2013, faisant un box-office de 406,9 millions $",
+        "dureeTotale" : 179,
+        "dureeVisio" : 150,
+        "imgUrl" : "assets/img/affiches_films/loup_de_wall_street.jpg",
+      },
+    ]
 
   }
 }
--- src/app/models/liste-films.model.ts
+++ src/app/models/liste-films.model.ts
@@ -14,12 +14,10 @@ export class Film {
     ) {}
 }
 
-//  const films = [
-//   new Films("Avatar", "James Cameron", 162, 162, "Film americain de science-fiction, réalisé en 2009"),
-//   new Films("Intouchables", "Olivier Nakache et Eric Toledano", 112, 112, "Comédie/drame français sorti en 2011"),
-//   new Films("Django Unchained", "Quentin Tarantino", 165, 120, "Western américain sorti en 2013"),
-//   new Films("Get Out", "Jordan Peele", 104, 104, "Ce thriller américain est sorti en 2017, faisant un box-office de 255,4 millions $"),
-//   new Films("Drunk", "Thomas Vinterberg", 115, 100, "Une comédie dramatique sorti en 2020, au Danemark"),
-//   new Films("Le loup de Wall Street", "Martin Scorsese", 179, 150, "Célèbre comédie dramatique américaine sorti en 2013, faisant un box-office de 406,9 millions $"),
-// ]       
+
+
+
+
+
+
 
